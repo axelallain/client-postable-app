50b499068c916d5674c2aa08e8bc126a8cd63918
axelallain <axel.allain.a@gmail.com>
2022-03-31 18:22:00 +0200

    Working on the top bar component.

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native'
 
 export default class TopBar extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>This is a top bar.</Text>
+            <View style={styles.container}>
+                <TouchableOpacity><Text style={styles.text}>Back</Text></TouchableOpacity>
+                <Text style={styles.text}>Logo</Text>
+                <TouchableOpacity><Text style={styles.text}>Menu</Text></TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      alignSelf: 'stretch',
+      height: 100,
+      flexDirection: 'row', // row
+      backgroundColor: '#064A04',
+      alignItems: 'center',
+      justifyContent: 'space-between', // center, space-around
+      paddingTop: 40,
+      paddingLeft: 25,
+      paddingRight: 25
+    },
+
+    text: {
+        color: 'white',
+        fontWeight: 'bold'
+    }
+  });
