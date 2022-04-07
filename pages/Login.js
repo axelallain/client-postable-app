4ea04526f40d18ae7490abbf22a0b99a952247f1
axelallain <axel.allain.a@gmail.com>
2022-04-07 18:43:08 +0200

    iOS logos and adding a dot in the login's title.

--- pages/Login.js
+++ pages/Login.js
@@ -58,7 +58,7 @@ const Login = props => {
   if(!user) {
     return (
       <View style={styles.container}>
-          <Text style={styles.name}>Postable</Text>
+          <Text style={styles.name}>Postable.</Text>
           <TouchableOpacity title="Google Sign-In" onPress={this.signInWithGoogle} style={styles.TouchableOpacity}><Text style={styles.loginText}>Se connecter avec Google</Text></TouchableOpacity>
           <Text style={styles.copyright}>Copyright 2022 - 2023 Postable. All rights reserved.</Text>
       </View>
